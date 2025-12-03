16653703e55a1e25e3d5aaf0cc816e92420a6676
rizkizikrillah <zikrullahriski@gmail.com>
2025-12-03 08:32:26 +0700

    model pengembalian

--- apk-inventarisasi/app/Models/Pengembalian.php
+++ apk-inventarisasi/app/Models/Pengembalian.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pengembalian extends Model
 {
-    //
+    protected $table = 'pengembalian';
+
+    protected $fillable = [
+        'peminjaman_id',
+        'tanggal_pengembalian',
+        'kondisi_barang',
+        'keterangan',
+        'foto_bukti',
+    ];
+
+    protected $casts = [
+        'tanggal_pengembalian' => 'datetime',
+    ];
+
+    public function peminjaman()
+    {
+        return $this->belongsTo(Peminjaman::class);
+    }
 }
